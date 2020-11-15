f66950422846d70258b29a86cccb608ccb43fd2e
sluetze <13255307+sluetze@users.noreply.github.com>
2020-11-15 05:24:24 -0500

    german translation finished
    Signed-off-by: sluetze <13255307+sluetze@users.noreply.github.com>

--- src/portal/src/app/shared/shared.const.ts
+++ src/portal/src/app/shared/shared.const.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-export const supportedLangs = ['en-us', 'zh-cn', 'zh-tw', 'es-es', 'fr-fr', 'pt-br', 'tr-tr', 'de-de'];
+export const supportedLangs = ['en-us', 'zh-cn', 'zh-tw', 'es-es', 'fr-fr', 'pt-br', 'tr-tr', 'de-de', 'ru-ru'];
 export const enLang = "en-us";
 export const languageNames = {
   "en-us": "English",
